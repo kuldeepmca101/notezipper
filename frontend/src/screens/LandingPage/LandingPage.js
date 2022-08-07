14a8b20ca872ffccde3fc5ce68232a3c12330034
Kuldeep Singh <kuldeep.mca10@gmail.com>
2022-08-07 10:06:14 +0530

    created new  header, footer and screen folder for landing page
    ref #master

---
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import './LandingPage.css';
+
+export default function LandingPage() {
+    return (
+        <div className="main">
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="intro-text">
+                            <div>
+                                <h1 className="title">Welocome to Note Zipper</h1>
+                                <p className="subtitle">One Safe Place for all your noted</p>
+                            </div>
+                        </div>
+                        <div className="buttonContainer">
+                            <a href="/login">
+                                <Button size="lg" className="landingButton">Login</Button>
+                            </a>
+                            <a href="/register">
+                                <Button className="landingButton" variant="outline-primary">Sign Up</Button>
+                            </a>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
